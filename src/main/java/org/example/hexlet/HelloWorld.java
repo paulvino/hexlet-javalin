4c8e4c3f9c6682c96fbae96b85ea3a4809abcd64
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-11-11 15:57:47 +0300

    Update HTTP session

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,12 +4,38 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
+
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+
+//        app.get("/", ctx -> {
+//            ctx.contentType("text/html");
+//            ctx.result("<h1>Hello Hexlet</h1>");
+//        });
+
+//        app.get("/users", ctx -> {
+//            ctx.result("GET /users");
+//        });
+//
+//        app.post("/users", ctx -> {
+//            ctx.result("POST /users");
+//        });
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+//            if (name != null) {
+//                ctx.result("Hello, " + name + "!");
+//            } else {
+//                ctx.result("Hello, World!");
+//            }
+//            Замена кода выше (5 строк) тернарным оператором
+            var msg = (name != null) ? "Hello, " + name + "!" : "Hello, World!";
+//            ctx.result(msg); // обычный вывод, без форматирования
+            ctx.contentType("text/html"); // вывод с форматированием в html
+            ctx.result("<h1>" + msg + "</h1>");
+        });
+
+        app.start(7070);
     }
 }
