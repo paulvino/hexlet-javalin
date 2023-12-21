4f4239fab64de470934958474bbc202a434f5142
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-12-21 08:10:41 +0300

    minor update HelloWorld

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -13,11 +13,9 @@ import com.zaxxer.hikari.HikariDataSource;
 
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-//import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.stream.Collectors;
@@ -40,10 +38,6 @@ public class HelloWorld {
         hikariConfig.setJdbcUrl("jdbc:h2:mem:hexlet_project;DB_CLOSE_DELAY=-1;");
 
         var dataSource = new HikariDataSource(hikariConfig);
-        var url = HelloWorld.class.getClassLoader().getResource("schema.sql");
-        var file = new File(url.getFile());
-//        var sql = Files.lines(file.toPath())
-//                .collect(Collectors.joining("\n"));
         var sql = readResourceFile("schema.sql");
 
         try (var connection = dataSource.getConnection();
